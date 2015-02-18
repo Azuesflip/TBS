ab3d961fc4a2fd9084b111f9337025fbee332a41
Azuesflip <azuesflip@gmail.com>
2015-02-17 19:33:46 -0700

    again and again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.paperclip_defaults = {
-			    :storage => :s3,
-			    :s3_credentials => {
-			      :bucket => ENV['AWS_BUCKET'],
-			      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-			      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+			    :storage = :s3,
+			    :s3_credentials = {
+			      :bucket = ENV['AWS_BUCKET'],
+			      :access_key_id = ENV['AWS_ACCESS_KEY_ID'],
+			      :secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
 			    },
-			    :path => ":class/:id/:basename_:style.:extension",
-			    :url => ":s3_sg_url"
+			    :path = ":class/:id/:basename_:style.:extension",
+			    :url = ":s3_sg_url"
 		  }
 end
