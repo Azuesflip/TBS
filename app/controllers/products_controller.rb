ed8bf9e8dbaf63eb0dfae6bf8298d7125b4fa091
Azuesflip <azuesflip@gmail.com>
2015-02-21 18:41:51 -0700

    image upload to s3 and dropdown fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,6 @@ class ProductsController < ApplicationController
 
   # GET /products/new
   def new
-  	@s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
     @product = Product.new
   end
 
